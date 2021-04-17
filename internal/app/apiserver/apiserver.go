02d9c69bee9bd67b7df405fee8785a889fd40b7f
Vadim Popov <vadim.popov@calaton.systems>
2021-04-17 23:20:42 +0300

    Server initialization

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,14 @@
+package apiserver
+
+//APIServer is
+type APIServer struct{}
+
+//New is ...
+func New() *APIServer {
+	return &APIServer{}
+}
+
+//Start is ...
+func (s *APIServer) Start() error {
+	return nil
+}
